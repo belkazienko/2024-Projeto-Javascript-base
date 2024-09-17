9a7c4928ae6c709ab741b5b3e5b73d51bdecc39c
Cibelly Kazienko <160873554+belkazienko@users.noreply.github.com>
2024-09-17 12:09:49 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+// Tipo Number
+
+// var era uma utilizado para todas as variáveis
+var numero01 = 3
+// let é uma atualização do var e é passível de variação
+const numero02 = 10
+//const é 
+const numero03 = 7
+
+const soma = numero01 + numero02
+const subtracao = numero02 - numero03
+const  multiplicacao = numero03 * numero01
+const divisao = numero01 / numero02
